32486b0fcffc0eaab6c132b3d04625bf198fb7f4
Jcabs129 <GitHub.com>
2018-05-30 22:09:55 +0100

    VR view tutorial

---
+++ vrView.js
@@ -0,0 +1,27 @@
+  window.addEventListener('load', onVrViewLoad);
+
+    function onVrViewLoad() {
+      // Selector '#vrview' finds element with id 'vrview'.
+      var vrView = new VRView.Player('#vrview', {
+        video: '/url/to/video.mp4',
+        is_stereo: true
+    });
+    }
+
+// Selector '#vrview' finds element with id 'vrview'.
+    var vrView = new VRView.Player('#vrview', {
+    video: '/url/to/video.mp4',
+    is_stereo: true
+    });
+
+    VRView.on('click', function(event) {
+    if (event.id == myHotspotId) {
+      // Handle hotspot click.
+    }
+    });
+
+    vrView.setContentInfo({
+    image: '/url/to/amazing-4096.jpg',
+    preview:'/url/to/amazing-512.jpg',
+    is_stereo: true
+    });
